59a570fa12ec964c0c1d6ee2338cdac630a8343a
Bubulle <bubulle86@yahoo.fr>
2025-03-09 00:53:14 +0100

    Adding sql files

---
+++ sql/delete-all.sql
@@ -0,0 +1,2 @@
+DELETE FROM public.report_updates;
+DELETE FROM public.reports;
---
+++ sql/drop-and-create.sql
@@ -0,0 +1,35 @@
+-- Table: public.report_updates
+
+DROP TABLE IF EXISTS public.report_updates;
+
+CREATE TABLE IF NOT EXISTS public.report_updates
+(
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    report_id bigint NOT NULL,
+    still_there boolean,
+	time timestamp with TIME zone NOT NULL,
+    CONSTRAINT report_updates_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.report_updates
+    OWNER to postgres;
+
+
+-- Table: public.reports
+
+DROP TABLE IF EXISTS public.reports;
+
+CREATE TABLE IF NOT EXISTS public.reports
+(
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    station_id bigint NOT NULL,
+    alive boolean NOT NULL DEFAULT true,
+    CONSTRAINT reports_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.reports
+    OWNER to postgres;
