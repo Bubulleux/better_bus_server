9fc8d5647d9d0dd5222db1e2db5fa08851919ac5
Bubulle <bubulle86@yahoo.fr>
2025-05-21 16:17:04 +0200

    Stupid Bug

--- lib/models/server_paths.dart
+++ lib/models/server_paths.dart
@@ -5,7 +5,9 @@ import 'package:better_bus_core/core.dart';
 class ServerPaths extends GTFSPaths {
   ServerPaths({Directory? root}) : super.broken(){
     root ??= Directory.current;
-    super.gtfsFilePath = "${root.path}/gtfs/gtfs.zip";
-    super.extractDir = "${root.path}/gtfs/extract/";
+    // Nerver add / before gtfs
+    // It'll produce error without tilling you
+    super.gtfsFilePath = "${root.path}gtfs/gtfs.zip";
+    super.extractDir = "${root.path}gtfs/extract/";
   }
 }
